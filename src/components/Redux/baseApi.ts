54b7a9576f03e109879cfe22bc3e9cfb9f166339
Prasanjit Sarker <sarkerprasanjit379@gmail.com>
2024-11-04 09:45:16 +0600

    Change Api Url

--- src/components/Redux/baseApi.ts
+++ src/components/Redux/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "./store";
 
 const baseQueryF = fetchBaseQuery({
-  baseUrl: process.env.API_URL,
+  baseUrl: process.env.NEXT_PUBLIC_API_URL,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.accessToken;
--- src/components/ServerRender/userLogin.tsx
+++ src/components/ServerRender/userLogin.tsx
@@ -4,7 +4,7 @@ import { FieldValues } from "react-hook-form";
 import { AuthKey } from "../Lib/AuthKey";
 
 export const userLogin = async (formData: FieldValues) => {
-  const res = await fetch(`${process.env.API_URL}/auth/login`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
